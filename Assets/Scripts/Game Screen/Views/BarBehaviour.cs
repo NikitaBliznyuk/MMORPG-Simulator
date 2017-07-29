eb3efe788ad98d9dafdde3c50319d899a6fe7ba7
Nikita Bliznyuk <nikbleid96@gmail.com>
2017-07-29 12:44:24 +0300

    Added ui view behaviour structure.

---
+++ Assets/Scripts/Game Screen/Views/BarBehaviour.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Game.View
+{
+    public class BarBehaviour : MonoBehaviour
+    {
+        [SerializeField] private Image view;
+        [SerializeField] private Text text;
+    }
+}
---
+++ Assets/Scripts/Game Screen/Views/GameUiView.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Game.View
+{
+    public class GameUiView : MonoBehaviour
+    {
+        [SerializeField] private UiBehaviour topUI;
+
+        private void Start()
+        {
+            topUI.gameObject.SetActive(false);
+        }
+
+        public void UpdateTopUi(UiInfo info, bool active)
+        {
+            topUI.gameObject.SetActive(active);
+            
+            
+        }
+    }
+
+    public struct UiInfo
+    {
+        public int Health;
+        public int Energy;
+        public string Name;
+    }
+}
