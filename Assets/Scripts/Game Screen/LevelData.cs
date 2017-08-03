fbb9808fe44a8d6377bf6af9ec77d4543e234821
Nikita Bliznyuk <nikbleid96@gmail.com>
2017-08-03 23:45:26 +0300

    Level Data refactored.

--- Assets/Scripts/Game Screen/LevelData.cs
+++ Assets/Scripts/Game Screen/LevelData.cs
@@ -1,9 +1,25 @@
 ﻿using UnityEngine;
+using CharacterInfo = Game.Character.CharacterInfo;
 
 [CreateAssetMenu(menuName = "Level Data")]
 public class LevelData : ScriptableObject
 {
-    public Game.Character.CharacterInfo Player;
-    public Game.Character.CharacterInfo[] Allies;
-    public Game.Character.CharacterInfo[] Enemies;
+    [SerializeField] private CharacterInfo player;
+    [SerializeField] private CharacterInfo[] allies;
+    [SerializeField] private CharacterInfo[] enemies;
+
+    public CharacterInfo Player
+    {
+        get { return player; }
+    }
+
+    public CharacterInfo[] Allies
+    {
+        get { return allies; }
+    }
+
+    public CharacterInfo[] Enemies
+    {
+        get { return enemies; }
+    }
 }
