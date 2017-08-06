846fec94d81869c69ef76f5af3c0b4edccbd0a4f
Nikita Bliznyuk <nikbleid96@gmail.com>
2017-08-07 02:38:58 +0300

    Added camera controller.

---
+++ Assets/Scripts/Game Screen/Camera/CameraController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [Header("Settings")] 
+    
+    [SerializeField] private float dampTime = 0.5f;
+    
+    private Transform currentTarget;
+    private Vector3 offset;
+    
+    private void Awake()
+    {
+        Loader.DataUpdated += LoaderOnDataUpdated;
+    }
+
+    private void LoaderOnDataUpdated(LevelCurrentData data)
+    {
+        offset = transform.position - data.PlayerReference.transform.position;
+        currentTarget = data.PlayerReference.transform;
+    }
+
+    private void LateUpdate()
+    {
+        if (currentTarget != null)
+        {
+            Vector3 currentVelocity = Vector3.zero;
+            transform.position = Vector3.SmoothDamp(transform.position, currentTarget.transform.position + offset,
+                ref currentVelocity, dampTime);
+        }
+    }
+}
