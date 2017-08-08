5291a9f6303686c1ad4d52bacc5f55e3c787cd1b
Nikita Bliznyuk <nikbleid96@gmail.com>
2017-08-08 09:54:22 +0300

    Implement generic state machine.

---
+++ Assets/Scripts/Game Screen/Character/State Machine/State.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu (menuName = "PluggableAI/State")]
+public class State : ScriptableObject 
+{
+
+	public Action[] actions;
+	public Transition[] transitions;
+	public Color sceneGizmoColor = Color.grey;
+
+	public void UpdateState(StateController controller)
+	{
+		DoActions (controller);
+		CheckTransitions (controller);
+	}
+
+	private void DoActions(StateController controller)
+	{
+		foreach (Action action in actions)
+		{
+			action.Act (controller);
+		}
+	}
+
+	private void CheckTransitions(StateController controller)
+	{
+		foreach (Transition transition in transitions)
+		{
+			bool decisionSucceeded = transition.decision.Decide (controller);
+
+			controller.TransitionToState(decisionSucceeded ? transition.trueState : transition.falseState);
+		}
+	}
+
+
+}
