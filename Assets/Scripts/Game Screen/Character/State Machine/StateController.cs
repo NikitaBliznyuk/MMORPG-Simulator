95a1a881703a4b35d554aa69b40ecc8e2ddf906e
Nikita Bliznyuk <nikbleid96@gmail.com>
2017-08-21 15:28:53 +0300

    Generate some documentation.

--- Assets/Scripts/Game Screen/Character/State Machine/StateController.cs
+++ Assets/Scripts/Game Screen/Character/State Machine/StateController.cs
@@ -1,51 +1,85 @@
-﻿using System;
-using System.Collections.Generic;
-using Game.Character;
+﻿using Game.Character;
 using UnityEngine;
 using UnityEngine.AI;
 
 [RequireComponent(typeof(NavMeshAgent))]
 [RequireComponent(typeof(CharacterInfoController))]
-public class StateController : MonoBehaviour {
-
+public class StateController : MonoBehaviour
+{
 	[Header("References")]
-	
-	[SerializeField] private State currentState;
-	[SerializeField] private State remainState;
-	
+	[SerializeField]
+	[Tooltip("Current character state.")]
+	private State currentState;
+
+	[SerializeField]
+	[Tooltip("Remain state.")]
+	private State remainState;
+
 	[Header("Settings")]
-	
-	[SerializeField] private Transform[] wayPointList;
-	[SerializeField] private float aggroRange;
+	[SerializeField]
+	[Tooltip("Positions in World Space. Character will move from one to another.")]
+	private Transform[] wayPointList;
 
-	[Header("Abilities")] 
-	
-	[SerializeField] private int[] simpleAttacksIndices;
+	[SerializeField]
+	[Tooltip("Range of aggro in World Space.")]
+	private float aggroRange;
 
+	[Header("Abilities")]
+	[SerializeField]
+	[Tooltip("Attack indices in abilities array.")]
+	private int[] simpleAttacksIndices;
+
+	/// <summary>
+	/// Next way point in way point list.
+	/// </summary>
 	private int nextWayPoint;
 
+	/// <summary>
+	/// Reference to CharacterInfoController instance. It contains all character info (abilities, stats, etc.).
+	/// </summary>
 	public CharacterInfoController InfoController { get; private set; }
-	public float AggroRange { get { return aggroRange; } }
+
+	/// <summary>
+	/// Range of aggro.
+	/// </summary>
+	public float AggroRange
+	{
+		get { return aggroRange; }
+	}
+
+	/// <summary>
+	/// Reference to chasing target.
+	/// </summary>
 	public Transform ChaseTarget { get; set; }
+
+	/// <summary>
+	/// Reference to NavMesh Agent.
+	/// </summary>
 	public NavMeshAgent NavMeshAgent { get; private set; }
 
 	#region Abilities getters
 
+	/// <summary>
+	/// Get indices of simple attacks from abilities array.
+	/// </summary>
 	public int[] SimpleAttacksIndices
 	{
 		get { return simpleAttacksIndices; }
 	}
 
 	#endregion
-	
-	private void Awake () 
+
+	private void Awake()
 	{
-		NavMeshAgent = GetComponent<NavMeshAgent> ();
+		NavMeshAgent = GetComponent<NavMeshAgent>();
 		InfoController = GetComponent<CharacterInfoController>();
-		
+
 		InfoController.StateInfo.ChangeState += StateInfoOnChangeState;
 	}
 
+	/// <summary>
+	/// Executes, when character change internal state.
+	/// </summary>
 	private void StateInfoOnChangeState()
 	{
 		enabled = InfoController.StateInfo.CurrentState != CharacterState.StateName.DEAD;
@@ -53,21 +87,28 @@ public class StateController : MonoBehaviour {
 
 	private void Update()
 	{
-		currentState.UpdateState (this);
+		currentState.UpdateState(this);
 	}
 
+	/// <summary>
+	/// Transition to derived state.
+	/// </summary>
+	/// <param name="nextState">Next state</param>
 	public void TransitionToState(State nextState)
 	{
-		if (nextState != remainState) 
+		if (nextState != remainState)
 		{
 			currentState = nextState;
-			OnExitState ();
+			OnExitState();
 		}
 	}
 
+	/// <summary>
+	/// Executes after changing state.
+	/// </summary>
 	private void OnExitState()
 	{
-		
+
 	}
 
 	#region Editor only
