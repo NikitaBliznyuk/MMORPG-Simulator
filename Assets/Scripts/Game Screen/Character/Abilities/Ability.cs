6ac4ecee697c82f2b9a1bd8e7fffbeb6bc337153
Nikita Bliznyuk <nikbleid96@gmail.com>
2017-09-01 16:47:23 +0300

    Some documentation.

--- Assets/Scripts/Game Screen/Character/Abilities/Ability.cs
+++ Assets/Scripts/Game Screen/Character/Abilities/Ability.cs
@@ -9,24 +9,56 @@ namespace GameScreen.Character.Abilities
     public abstract class Ability : ScriptableObject
     {
         [SerializeField]
+        [Tooltip("Ability information.")]
         private AbilityInfo abilityInfo;
 
+        /// <summary>
+        /// Reference to ability information (read + write).
+        /// </summary>
         public AbilityInfo AbilityInfo
         {
             get { return abilityInfo; }
             set { abilityInfo = value; }
         }
 
+        /// <summary>
+        /// Abstract method for all abilities.
+        /// </summary>
+        /// <param name="invoker">Invoker.</param>
+        /// <param name="target">Target.</param>
+        /// <returns>Ability invoke code. <seealso cref="AbilityInvokeErrorCode"/>></returns>
         public abstract AbilityInvokeErrorCode Invoke(CharacterInfoController invoker, CharacterInfoController target);
     }
 
+    /// <summary>
+    /// Ability information.
+    /// </summary>
     [Serializable]
     public struct AbilityInfo
     {
+        /// <summary>
+        /// Ability name.
+        /// </summary>
         public string Name;
+
+        /// <summary>
+        /// Ability desctition. Few sentencies.
+        /// </summary>
         public string Description;
+
+        /// <summary>
+        /// Ability cooldown in seconds.
+        /// </summary>
         public float Cooldown;
+
+        /// <summary>
+        /// Ability energy cost.
+        /// </summary>
         public int Cost;
+
+        /// <summary>
+        /// Ability cast distance in Unity units.
+        /// </summary>
         public float CastDistance;
     }
 
--- Assets/Scripts/Game Screen/Character/Abilities/HitAbility.cs
+++ Assets/Scripts/Game Screen/Character/Abilities/HitAbility.cs
@@ -3,34 +3,41 @@ using UnityEngine;
 
 namespace GameScreen.Character.Abilities
 {
-	[CreateAssetMenu(menuName = "Abilities/Hit ability")]
-	public class HitAbility : Ability, IHit
-	{
-		[SerializeField]
-		private HitInfo hitInfo;
+    /// <summary>
+    /// Basic hit ability (like weapon attack).
+    /// </summary>
+    [CreateAssetMenu(menuName = "Abilities/Hit ability")]
+    public class HitAbility : Ability, IHit
+    {
+        [SerializeField]
+        [Tooltip("Hit information.")]
+        private HitInfo hitInfo;
 
-		public HitInfo HitInfo
-		{
-			get { return hitInfo; }
-			set { hitInfo = value; }
-		}
+        /// <summary>
+        /// Reference to hit information (read + write).
+        /// </summary>
+        public HitInfo HitInfo
+        {
+            get { return hitInfo; }
+            set { hitInfo = value; }
+        }
 
-		public override AbilityInvokeErrorCode Invoke(CharacterInfoController invoker, CharacterInfoController target)
-		{
-			if (target == null || target.StateInfo.CurrentState == CharacterState.StateName.DEAD)
-				return AbilityInvokeErrorCode.WRONG_TARGET;
+        public override AbilityInvokeErrorCode Invoke(CharacterInfoController invoker, CharacterInfoController target)
+        {
+            if (target == null || target.StateInfo.CurrentState == CharacterState.StateName.DEAD)
+                return AbilityInvokeErrorCode.WRONG_TARGET;
 
-			if (!invoker.Info.EnemyTags.Contains(target.Info.Tag))
-				return AbilityInvokeErrorCode.WRONG_TARGET;
+            if (!invoker.Info.EnemyTags.Contains(target.Info.Tag))
+                return AbilityInvokeErrorCode.WRONG_TARGET;
 
-			if (Vector3.Distance(invoker.transform.position, target.transform.position) - (invoker.Size + target.Size) >
-			    AbilityInfo.CastDistance)
-				return AbilityInvokeErrorCode.TOO_FAR;
+            if (Vector3.Distance(invoker.transform.position, target.transform.position) - (invoker.Size + target.Size) >
+                AbilityInfo.CastDistance)
+                return AbilityInvokeErrorCode.TOO_FAR;
 
-			int hitValue = Random.Range(hitInfo.MinDamage, hitInfo.MaxDamage + 1);
-			target.DealDamage(hitValue);
+            int hitValue = Random.Range(hitInfo.MinDamage, hitInfo.MaxDamage + 1);
+            target.DealDamage(hitValue);
 
-			return AbilityInvokeErrorCode.NO_ERROR;
-		}
-	}
+            return AbilityInvokeErrorCode.NO_ERROR;
+        }
+    }
 }
